d5c6ab5f415d86ff65b760977d70c4e009ffa541
milan <milan@example.com>
2019-01-10 19:32:13 +0100

    toolbar added

--- app/src/main/java/com/example/milan/vezbanje1/MainActivity.java
+++ app/src/main/java/com/example/milan/vezbanje1/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example.milan.vezbanje1;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +21,66 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+
+
+
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //! Add
+        getMenuInflater().inflate(R.menu.menu_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+
+
+            case R.id.action_settings:
+
+               Intent intent = new Intent(MainActivity.this,
+                        PreferenceSettingsActivity.class);
+                startActivity(intent);
+
+                return true;
+
+            case R.id.action_about:
+                AlertDialog.Builder basicAlertDialogBuilder = new AlertDialog.Builder(this);
+                basicAlertDialogBuilder.setMessage(R.string.alert_dialog);
+                basicAlertDialogBuilder.setPositiveButton(R.string.alert_button_ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Snackbar snackbar = Snackbar.make(findViewById(R.id.main_activity), "MM", Snackbar.LENGTH_LONG);
+                        snackbar.show();
+                    }
+                });
+                basicAlertDialogBuilder.create().show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        }
+
+
+
+
 
 
 }
---
+++ app/src/main/java/com/example/milan/vezbanje1/PreferenceSettingsActivity.java
@@ -0,0 +1,44 @@
+package com.example.milan.vezbanje1;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+
+public class PreferenceSettingsActivity extends PreferenceActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new PreferenceSettingsActivity.PrefsFragment()).commit();
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(PreferenceSettingsActivity.this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public static class PrefsFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.moj_preference);
+        }
+
+    }
+}
